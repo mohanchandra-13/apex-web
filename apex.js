2b210c3f4878632eda97e9ba501528c106b25828
mohanchandra-13 <thumu.mohan.chandra@gmail.com>
2025-08-02 21:54:17 +0530

    Initial commit

---
+++ apex.js
@@ -0,0 +1,9 @@
+// Find the button by its ID
+const myButton = document.getElementById('alertButton');
+
+// Add a "click" listener to the button
+myButton.addEventListener('click', () => {
+    // Show an alert message when the button is clicked
+    alert('Your next adventure awaits!');
+});
+
